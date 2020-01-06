45eca0d8d8760a652651f4afd3307dc26832ab93
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-06 17:20:17 +0100

    Validate dependencies

---
+++ src/semantictypes/StaticTests/DependencyTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeGeneration.Roslyn;
+using FluentAssertions;
+using Obviously.SemanticTypes.Generator;
+using Xunit;
+
+namespace Obviously.SemanticTypes.StaticTests
+{
+    public class DependencyTests
+    {
+        [Fact]
+        public void GivenTheCompiledAssembly_WhenCheckingItsDependencies_ThenThese_ShouldNotContainTheGeneratorAssemblies()
+        {
+            var notReferencedAssemblyNames = new[]
+            {
+                typeof(SemanticTypeGenerator).Assembly.GetName(),
+                typeof(IRichCodeGenerator).Assembly.GetName(),
+            };
+            typeof(DependencyTests).Assembly.GetReferencedAssemblies().Should().NotContain(notReferencedAssemblyNames);
+        }
+    }
+}

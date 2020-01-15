03cf808925b187c728bbf2e65e67c243a2f070d6
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-15 21:58:54 +0100

    Read types with more than one property

---
+++ src/system.text.json/Core.Tests/Converter_Read_ComplexType_Behavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Obviously.System.Text.Json.Tests
+{
+    public class Converter_Read_ComplexType_Behavior
+    {
+        private static readonly JsonSerializerOptions Settings;
+
+        static Converter_Read_ComplexType_Behavior()
+        {
+            Settings = new JsonSerializerOptions();
+            Settings.Converters.Add(new ImmutableConverter());
+        }
+
+        [Theory]
+        [InlineData(@"{ ""Foo"": ""bar"", ""Xyz"": ""bla""}")]
+        [InlineData(@"{ ""Xyz"": ""bla"", ""Foo"": ""bar""}")]
+        public void GivenOneValidTypeWithTwoProperties_WhenDeserializing_ThenTheObject_ShouldContainTheCorrectPropertyValues(string json)
+        {
+            var deserializeObject = JsonSerializer.Deserialize<TestingTypeComplexMatchingTypes.Basic>(json, Settings);
+            using var _ = new AssertionScope();
+            deserializeObject.Foo.Should().Be("bar");
+            deserializeObject.Xyz.Should().Be("bla");
+        }
+
+        [Theory]
+        [InlineData(@"{ ""Xyz"": ""bla"", ""Foo"": ""notbar"", ""Foo"": ""bar""}")]
+        [InlineData(@"{ ""Foo"": ""notbar"", ""Xyz"": ""bla"",  ""Foo"": ""bar""}")]
+        [InlineData(@"{ ""Foo"": ""notbar"", ""Foo"": ""bar"", ""Xyz"": ""bla""}")]
+        public void GivenOneValidTypeWithTwoProperties_AndOneJsonContainingOnePropertyTwice_WhenDeserializing_ThenTheCorrespondingPropertyOfTheObject_ShouldContainTheCorrectValue(string json)
+        {
+            var deserializeObject = JsonSerializer.Deserialize<TestingTypeComplexMatchingTypes.Basic>(json, Settings);
+            using var _ = new AssertionScope();
+            deserializeObject.Foo.Should().Be("bar");
+            deserializeObject.Xyz.Should().Be("bla");
+        }
+
+        [Theory]
+        [InlineData(@"{ ""Foo"": ""bar"", ""Xyz"": ""bla"", ""NotInteresting"": ""__""}")]
+        [InlineData(@"{ ""NotInteresting"": ""__"", ""Foo"": ""bar"", ""Xyz"": ""bla""}")]
+        public void GivenOneValidTypeWithTwoProperties_AndOneJsonWithAnAdditionalProperty_WhenDeserializing_ThenTheObject_ShouldContainTheCorrectPropertyValues(string json)
+        {
+            var deserializeObject = JsonSerializer.Deserialize<TestingTypeComplexMatchingTypes.Basic>(json, Settings);
+            using var _ = new AssertionScope();
+            deserializeObject.Foo.Should().Be("bar");
+            deserializeObject.Xyz.Should().Be("bla");
+        }
+    }
+}
---
+++ src/system.text.json/Core.Tests/TestingTypeComplexMatchingTypes.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Obviously.System.Text.Json.Tests
+{
+#pragma warning disable IDE0060 // Remove unused parameter
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+    public static class TestingTypeComplexMatchingTypes
+    {
+        public sealed class Basic
+        {
+            public Basic(string foo, string xyz)
+            {
+                Foo = foo;
+                Xyz = xyz;
+            }
+
+            public string Foo { get; }
+
+            public string Xyz { get; }
+        }
+    }
+}

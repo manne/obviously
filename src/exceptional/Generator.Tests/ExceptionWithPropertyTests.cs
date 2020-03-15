69475ee3e114d40773d9e2156b8803151b3c2b9e
Manuel Pfemeter <m>
2020-03-15 16:31:18 +0100

    exceptional: Work with properties

---
+++ src/exceptional/Generator.Tests/ExceptionWithPropertyTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Obviously.Exceptional.Generator;
+using Xunit;
+
+namespace Generator.Tests
+{
+    [Exceptional]
+    public sealed partial class ExceptionWithPropertyException : Exception
+    {
+        public string Foo { get; }
+        public int Bar { get; }
+    } 
+
+    public class ExceptionWithPropertyTests
+    {
+        [Fact]
+        public void ExceptionTypesWithCustomProperties_ShouldBeUsable()
+        {
+            _ = new ExceptionWithPropertyException();
+        }
+    }
+}
--- src/exceptional/Generator/ExceptionGenerator.cs
+++ src/exceptional/Generator/ExceptionGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -27,21 +28,90 @@ namespace Obviously.Exceptional.Generator
             var classSymbol = context.SemanticModel.GetDeclaredSymbol(applyToClass);
             var idName = classSymbol.Name;
 
-            var constructors = new List<ConstructorDeclarationSyntax>(3)
+            var propertyInfos = GetPropertyInfos(applyToClass);
+
+            var members = new List<MemberDeclarationSyntax>(5)
             {
                 GetConstructorDefault(idName),
                 GetConstructorWithMessage(idName),
-                GetConstructorWithMessageAndInnerException(idName)
+                GetConstructorWithMessageAndInnerException(idName),
+                GetConstructorForSerialization(idName, propertyInfos),
+                GetObjectDataOverride(propertyInfos)
             };
 
             var baseList = applyToClass.DescendantNodes().OfType<BaseListSyntax>().FirstOrDefault();
             var classWithoutBaseList = applyToClass.RemoveNode(baseList, SyntaxRemoveOptions.KeepNoTrivia);
 
-
-            var a = classWithoutBaseList
+            var finalClass = classWithoutBaseList
                 .WithAttributeLists(new SyntaxList<AttributeListSyntax>())
-                .WithMembers(new SyntaxList<MemberDeclarationSyntax>(constructors));
-            return Task.FromResult(new SyntaxList<MemberDeclarationSyntax>(a));
+                .WithMembers(new SyntaxList<MemberDeclarationSyntax>(members));
+            return Task.FromResult(new SyntaxList<MemberDeclarationSyntax>(finalClass));
+        }
+
+        private static IImmutableDictionary<string, string> GetPropertyInfos(ClassDeclarationSyntax applyToClass)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var properties = applyToClass.DescendantNodes().OfType<PropertyDeclarationSyntax>();
+            foreach (var property in properties)
+            {
+                var name = property.Identifier.ValueText;
+                var typeSyntax = (PredefinedTypeSyntax)property.Type;
+                dictionary.Add(name, typeSyntax.Keyword.ValueText);
+            }
+
+            return dictionary.ToImmutableDictionary();
+        }
+
+        private static MethodDeclarationSyntax GetObjectDataOverride(IImmutableDictionary<string, string> propertiesInfo)
+        {
+            var assignments = new List<ExpressionStatementSyntax>(propertiesInfo.Count + 1)
+            {
+                ExpressionStatement(
+                    InvocationExpression(
+                            MemberAccessExpression(
+                                SyntaxKind.SimpleMemberAccessExpression,
+                                BaseExpression(),
+                                IdentifierName("GetObjectData")))
+                        .WithArgumentList(
+                            ArgumentList(
+                                SeparatedList<ArgumentSyntax>(
+                                    new SyntaxNodeOrToken[]
+                                    {
+                                        Argument(
+                                            IdentifierName("info")),
+                                        Token(SyntaxKind.CommaToken),
+                                        Argument(
+                                            IdentifierName("context"))
+                                    }))))
+            };
+
+            foreach (var (name, typeName) in propertiesInfo)
+            {
+                var assignment = GetPropertySerializationAssignment(name, typeName);
+                assignments.Add(assignment);
+            }
+
+            var method = MethodDeclaration(
+                    PredefinedType(
+                        Token(SyntaxKind.VoidKeyword)),
+                    Identifier("GetObjectData"))
+                .WithModifiers(
+                    TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.OverrideKeyword)))
+                .WithParameterList(
+                    ParameterList(
+                        SeparatedList<ParameterSyntax>(
+                            new SyntaxNodeOrToken[]{
+                                Parameter(
+                                        Identifier("info"))
+                                    .WithType(
+                                        IdentifierName("global::System.Runtime.Serialization.SerializationInfo")),
+                                Token(SyntaxKind.CommaToken),
+                                Parameter(
+                                        Identifier("context"))
+                                    .WithType(
+                                        IdentifierName("global::System.Runtime.Serialization.StreamingContext"))})))
+                .WithBody(Block(assignments));
+            return method;
         }
 
         private static ConstructorDeclarationSyntax GetConstructorDefault(string idName)
@@ -124,5 +194,131 @@ namespace Obviously.Exceptional.Generator
                 .WithBody(Block());
             return constructor;
         }
+
+        private static ConstructorDeclarationSyntax GetConstructorForSerialization(string idName, IImmutableDictionary<string, string> propertiesInfo)
+        {
+            var propertyAssignments = new List<ExpressionStatementSyntax>(propertiesInfo.Count);
+            foreach (var (name, typeName) in propertiesInfo)
+            {
+                var assignment = GetPropertyDeserializationAssignment(name, typeName);
+                propertyAssignments.Add(assignment);
+            }
+
+            var constructor = ConstructorDeclaration(
+                    Identifier(idName))
+                .WithModifiers(
+                    TokenList(
+                        Token(
+                            TriviaList(
+                                Trivia(
+                                    PragmaWarningDirectiveTrivia(
+                                            Token(SyntaxKind.DisableKeyword),
+                                            true)
+                                        .WithErrorCodes(
+                                            SingletonSeparatedList<ExpressionSyntax>(
+                                                IdentifierName(
+                                                    Identifier(
+                                                        TriviaList(),
+                                                        "CS0628",
+                                                        TriviaList(
+                                                            Comment("// New protected member declared in sealed class")))))))),
+                            SyntaxKind.ProtectedKeyword, TriviaList())))
+                .WithParameterList(
+                    ParameterList(
+                        SeparatedList<ParameterSyntax>(
+                            new SyntaxNodeOrToken[]{
+                                Parameter(
+                                        Identifier("info"))
+                                    .WithType(
+                                        IdentifierName("global::System.Runtime.Serialization.SerializationInfo")),
+                                Token(SyntaxKind.CommaToken),
+                                Parameter(
+                                        Identifier("context"))
+                                    .WithType(
+                                        IdentifierName("global::System.Runtime.Serialization.StreamingContext"))})))
+                .WithInitializer(
+                    ConstructorInitializer(
+                            SyntaxKind.BaseConstructorInitializer,
+                            ArgumentList(
+                                SeparatedList<ArgumentSyntax>(
+                                    new SyntaxNodeOrToken[]{
+                                        Argument(
+                                            IdentifierName("info")),
+                                        Token(SyntaxKind.CommaToken),
+                                        Argument(
+                                            IdentifierName("context"))})))
+                        .WithColonToken(
+                            Token(
+                                TriviaList(
+                                    Trivia(
+                                        PragmaWarningDirectiveTrivia(
+                                                Token(SyntaxKind.RestoreKeyword),
+                                                true)
+                                            .WithErrorCodes(
+                                                SingletonSeparatedList<ExpressionSyntax>(
+                                                    IdentifierName(
+                                                        Identifier(
+                                                            TriviaList(),
+                                                            "CS0628",
+                                                            TriviaList(
+                                                                Comment("// New protected member declared in sealed class")))))))),
+                                SyntaxKind.ColonToken,
+                                TriviaList())))
+                .WithBody(Block(propertyAssignments));
+            return constructor;
+        }
+
+        private static ExpressionStatementSyntax GetPropertyDeserializationAssignment(string name, string typeName)
+        {
+            var assignment = ExpressionStatement(
+                AssignmentExpression(
+                    SyntaxKind.SimpleAssignmentExpression,
+                    IdentifierName(name),
+                    CastExpression(
+                            IdentifierName(typeName),
+                        InvocationExpression(
+                                MemberAccessExpression(
+                                    SyntaxKind.SimpleMemberAccessExpression,
+                                    IdentifierName("info"),
+                                    IdentifierName("GetValue")))
+                            .WithArgumentList(
+                                ArgumentList(
+                                    SeparatedList<ArgumentSyntax>(
+                                        new SyntaxNodeOrToken[]
+                                        {
+                                            Argument(
+                                                LiteralExpression(
+                                                    SyntaxKind.StringLiteralExpression,
+                                                    Literal(name))),
+                                            Token(SyntaxKind.CommaToken),
+                                            Argument(
+                                                TypeOfExpression(IdentifierName(typeName)))
+                                        }))))));
+            return assignment;
+        }
+
+        private static ExpressionStatementSyntax GetPropertySerializationAssignment(string name, string typeName)
+            => ExpressionStatement(
+                InvocationExpression(
+                        MemberAccessExpression(
+                            SyntaxKind.SimpleMemberAccessExpression,
+                            IdentifierName("info"),
+                            IdentifierName("AddValue")))
+                    .WithArgumentList(
+                        ArgumentList(
+                            SeparatedList<ArgumentSyntax>(
+                                new SyntaxNodeOrToken[]
+                                {
+                                    Argument(
+                                        LiteralExpression(
+                                            SyntaxKind.StringLiteralExpression,
+                                            Literal(name))),
+                                    Token(SyntaxKind.CommaToken),
+                                    Argument(
+                                        IdentifierName(name)),
+                                    Token(SyntaxKind.CommaToken),
+                                    Argument(
+                                        TypeOfExpression(IdentifierName(typeName)))
+                                }))));
     }
 }
